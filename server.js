163f974f821988b0ed1c1fee9c1ea071b21d3ee2
Tomson <tomsongrandy@gmail.com>
2024-10-13 16:26:30 +0300

    Ports

--- server.js
+++ server.js
@@ -2,7 +2,7 @@
 require('dotenv').config()
 const express = require('express');
 const app = express();
-const port = process.env.port || 4000;
+const port = process.env.PORT;
 const bodyParser = require('body-parser')
 const cors = require('cors');
 const mongoose = require('mongoose');
